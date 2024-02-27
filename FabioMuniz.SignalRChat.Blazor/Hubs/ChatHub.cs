6cbe6185eaa85baa2535e0d2dad1c4383607d3c4
Fabio Muniz <fabio.muniz@outlook.com>
2024-02-27 11:17:22 -0300

    first commit

---
+++ FabioMuniz.SignalRChat.Blazor/Hubs/ChatHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace FabioMuniz.SignalRChat.Blazor.Hubs;
+
+public class ChatHub : Hub
+{
+	public async Task SendMessage(string user, string message) => await Clients.All.SendAsync("ReceiveMessage", user, message);
+}
